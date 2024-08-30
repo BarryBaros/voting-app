cad2601544bd4102279b9914faa796338b40cfbd
Barasa <timbaros87@gmail.com>
2024-08-30 18:44:32 +0300

    Login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,6 +10,27 @@ function Login({ onLogin }) {
     };
 
     return (
-        
-    )
+        <div className="auth-container">
+            <h2>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Id Number:</label>
+                <input
+                    type="text"
+                    value={idNumber}
+                    onChange={(e) => setidNumber(e.target.value)}
+                    required />
+                    <label>Password:</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required />
+
+                        <button type="submit">Submit</button>
+            </form>
+            <p>Don't have an account? <span onClick={onLogin}>Register</span></p>
+            </div>
+    );
 }
+
+export default Login;
