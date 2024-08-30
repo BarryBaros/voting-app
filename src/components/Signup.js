bb065b614e1b4167663a7668ae2ca413b75e887f
Barasa <timbaros87@gmail.com>
2024-08-30 19:30:42 +0300

    create signup form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -15,5 +15,51 @@ function Signup({ onSignup }) {
             return;
         }
         onSignup(); //Perfom Signup here
-    }
+    };
+
+    return (
+        <div className="auth-container">
+            <h2>Sign-up</h2>
+            <form onSubmit={handleSubmit}>
+                <label>First Name:</label>
+                <input 
+                type="text"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                required
+                />
+                <label>Last Name:</label>
+                <input
+                type="text"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                required
+                />
+                <label>Year of Birth</label>
+                <input
+                type="text"
+                value={yearOfBirth}
+                onChange={(e) => setYearOfBirth(e.target.value)}
+                required
+                />
+                <label>Password:</label>
+                <input
+                type="passwor"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                />
+                <label>Confirm Password:</label>
+                <input
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
 }
+
+export default Signup;
