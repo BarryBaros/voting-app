986565d3b169780b9961a5c4cc793a0fcefceeb7
Barasa <timbaros87@gmail.com>
2024-08-30 18:59:35 +0300

    Create Signup function

---
+++ src/components/Signup.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+
+function Signup({ onSignup }) {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [idNumber, setidNumber] = useState('');
+    const [yearOfBirth, setYearOfBirth] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const handleSub,
+}
