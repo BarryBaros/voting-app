95d8b395787c8e90c6392a897a5e095564a88972
Barasa <timbaros87@gmail.com>
2024-09-09 05:10:52 +0300

    create Candidate model

--- server/app/models.py
+++ server/app/models.py
@@ -6,3 +6,8 @@ class Voter(db.Model):
     id_number = db.Column(db.Integer(20), unique=True, nullable=False)
     year_of_birth = db.Column(db.Integer, nullable=False)
     password = db.Column(db.String(100), nullable=False)
+
+class Candidate(db.Mondel):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    position = db.Column(db.String(100), nullable=False)
