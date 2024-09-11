d973925463610d34b06f10ea0635085b81dd6d84
MartinWakaba <martin.wakaba@student.moringaschool.com>
2024-09-11 23:09:50 +0300

    made server changes

--- server/models.py
+++ server/models.py
@@ -1,6 +1,8 @@
 from app import db
 
 class Voter(db.Model):
+    __tablename__ = "voters"
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     id_number = db.Column(db.Integer, unique=True, nullable=False)
@@ -8,17 +10,24 @@ class Voter(db.Model):
     password = db.Column(db.String(100), nullable=False)
 
 class Candidate(db.Model):
+    __tablename__ = "candidates"
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     position = db.Column(db.String(100), nullable=False)
     party = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.File, nullable=False)
 
 class Vote(db.Model):
+    __tablename__ = "vote"
+
     id = db.Column(db.Integer, primary_key=True)
-    Voter_id = db.Column(db.Integer, db.ForeignKey('voter.id'))
-    candidate_id = db.Column(db.Integer, db.ForeignKey('candidate.id'))
+    Voter_id = db.Column(db.Integer, db.ForeignKey('voters.id'))
+    candidate_id = db.Column(db.Integer, db.ForeignKey('candidates.id'))
 
 class Admin(db.Model):
+    __tablename__ = "admins"
+
     id  = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     id_numeber = db.Column(db.Integer, nullable=False)
